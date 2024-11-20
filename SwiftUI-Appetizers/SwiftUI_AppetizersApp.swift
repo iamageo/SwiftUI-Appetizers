13698e96e322d9176a4703c4344510b40c81fe06
iamageo <geovani.af4@gmail.com>
2024-11-20 19:45:40 -0300

    Initial Commit

---
+++ SwiftUI-Appetizers/SwiftUI_AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppetizersApp.swift
+//  SwiftUI-Appetizers
+//
+//  Created by Geovani Amaral on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-AppetizersTests/SwiftUI_AppetizersTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppetizersTests.swift
+//  SwiftUI-AppetizersTests
+//
+//  Created by Geovani Amaral on 20/11/24.
+//
+
+import Testing
+@testable import SwiftUI_Appetizers
+
+struct SwiftUI_AppetizersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
